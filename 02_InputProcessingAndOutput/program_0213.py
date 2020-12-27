5748fdae711ed66754f497f7338e570956b8f443
gesco <gescobedoz@gmail.com>
2020-12-27 11:54:23 -0600

    user input with float and int

---
+++ 02_InputProcessingAndOutput/program_0213.py
@@ -0,0 +1,19 @@
+# Get the user's name, age, and income.
+name = input('What is your name? ')
+age = int(input('What is your age? '))
+income = float(input('What is you income? '))
+
+# Display the data
+print('Here is the data you entered:')
+print('Name:', name)
+print('Age:', age)
+print('Income:', income)
+
+# OUTPUT
+# What is your name? Chris
+# What is your age? 25
+# What is you income? 75000.0
+# Here is the data you entered:
+# Name: Chris
+# Age: 25
+# Income: 75000.0
